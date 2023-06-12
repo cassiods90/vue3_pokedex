9737b6d04e6f044a392ed9b4ffb13cfbd7c9b696
cassiods90 <cassiods90@hotmail.com>
2023-06-12 23:15:56 +0100

    feat: Start a new project with initial build

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Nuxt 3 Home</h1>
+
+        <br /><br />
+
+        <HomePiniaExample />
+
+        <br /><br />
+
+        <HomeSwiperCarouselExample />
+
+        <br /><br />
+    </div>
+</template>
+
+<script setup></script>
