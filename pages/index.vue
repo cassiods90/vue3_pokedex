804b213542188bdf0d0d8a3d156905a78c296d4f
cassiods90 <cassiods90@hotmail.com>
2023-06-14 13:57:14 +0100

    Layout: Home page layout responsively

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="pages home">
         <div class="row">
-            <div class="col-12 col-sm-6">
+            <div class="col-12 col-xl-6">
                 <HomeTextProject />
             </div>
-            <div class="col-12 col-sm-6">
+            <div class="col-12 col-xl-6">
                 <HomePokesCarousel />
             </div>
         </div>
