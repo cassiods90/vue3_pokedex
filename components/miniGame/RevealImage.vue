f613500e1fae9488bdbf2a89c6d110d5f6abf637
cassiods90 <cassiods90@hotmail.com>
2023-07-02 00:44:47 +0100

    Feat: image fix

--- components/miniGame/RevealImage.vue
+++ components/miniGame/RevealImage.vue
@@ -1,10 +1,10 @@
 <template>
     <NuxtLink class="poke-image corretName" v-if="pokeRandom.correctName" :to="`/search/${pokeRandom.randomPoke.pokeId}`">
-        <img class="poke-background" src="@/assets/image/minigame/imagem3.png" />
+        <img class="poke-background" src="@/assets/image/minigame/imageBackground.png" />
         <img class="poke-img" :src="pokeRandom.randomPoke.PokeImage" />
     </NuxtLink>
     <div class="poke-image" v-else>
-        <img class="poke-background" src="@/assets/image/minigame/imagem3.png" />
+        <img class="poke-background" src="@/assets/image/minigame/imageBackground.png" />
         <img class="poke-img" :src="pokeRandom.randomPoke.PokeImage" />
     </div>
 </template>
